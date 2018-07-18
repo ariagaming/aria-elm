9131663d0d5d426c99cf932d06ca5d1b255aa510
Carlos <ckelkboom@gmail.com>
2018-07-18 07:24:43 +0200

    Equipment now has a dialog as well

--- src/Views/Equipment.elm
+++ src/Views/Equipment.elm
@@ -1,23 +1,43 @@
 module Views.Equipment exposing (..)
 
-import Html exposing (Html, Attribute, text, div, span, img, input, label)
+import Html exposing (Html, Attribute, text, div, span, img, input, label, i)
 import Html.Attributes exposing (class, style, src)
-import List exposing (map)
+import Html.Events exposing (onClick)
+import List exposing (map, concat, append, singleton)
 import Models exposing (..)
+
+
+{-
+
+   append to the head of a list
+   (::)  : a -> List a -> List a
+
+-}
+
+
+(:>) : List a -> a -> List a
+(:>) l a =
+    append l (singleton a)
 
 
 renderEquipment : Armors -> Html Msg
 renderEquipment (Armors armors) =
-    div
-        [ class "content-equipment" ]
-        (map
-            (\(Armor armor) ->
-                div [ class "equipment-row" ]
-                    [ span [ class "location" ] [ text <| toString armor.location ]
-                    ]
+    let
+        children =
+            (map
+                (\(Armor armor) ->
+                    div [ class "equipment-row" ]
+                        [ span [ class "location" ] [ text <| toString armor.location ]
+                        ]
+                )
+                armors
             )
-            armors
-        )
+                :> div [ class "nav-button", onClick (HideDialog EquipmentDialog) ]
+                    [ i [ class "fas fa-pencil-alt" ] [] ]
+    in
+        div
+            [ class "content-equipment" ]
+            children
 
 
 
